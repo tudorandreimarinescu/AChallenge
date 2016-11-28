e15913fa550930de371caa65db28e2124f5dbeb3
Andrei Marinescu <tudorandrei.marinescu@gmail.com>
2016-11-28 14:26:35 +0200

    #1

---
+++ AChallenge/AChallengeTests/AChallengeTests.swift
@@ -0,0 +1,36 @@
+//
+//  AChallengeTests.swift
+//  AChallengeTests
+//
+//  Created by Andrei Marinescu on 28/11/2016.
+//  Copyright © 2016 Tudor Andrei Marinescu. All rights reserved.
+//
+
+import XCTest
+@testable import AChallenge
+
+class AChallengeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
